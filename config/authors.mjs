03bc2c34d918eb3c8925b4545c0282c32c4c2e5e
suddenlysixam <suddenlysixam@gmail.com>
2026-09-10 07:08:24 -0400

    Moving avatars to public

--- config/authors.mjs
+++ config/authors.mjs
@@ -5,31 +5,31 @@ export const authors = {
     Megan_Steeley: {
         name: 'Megan Steeley',
         title: 'Advisor',
-        picture: '/src/assets/images/avatars/megan-steeley.jpg',
+        picture: '/images/avatars/megan-steeley.jpg',
         // url: ,
     },
     Joe_Ridge: {
         name: 'Joe Ridge',
-        picture: '/src/assets/images/avatars/joe-ridge.jpg'
+        picture: '/images/avatars/joe-ridge.jpg'
     },
     Stephen_Eide: {
         name: 'Stephen Eide',
-        picture: '/src/assets/images/avatars/stephen-eide.jpg'
+        picture: '/images/avatars/stephen-eide.jpg'
     },
     Nick_Curtis: {
         name: 'Nick Curtis',
         title: 'Alum, Former President',
-        picture: '/src/assets/images/avatars/nick-curtis.jpg'
+        picture: '/images/avatars/nick-curtis.jpg'
     },
     Jason_Pang: {
         name: 'Jason Pang',
         title: 'President',
-        picture: '/src/assets/images/avatars/jason-pang.jpg'
+        picture: '/images/avatars/jason-pang.jpg'
     },
     Clara_Gong: {
         name: 'Clara Gong',
         title: 'Alum, Former Developer',
-        picture: '/src/assets/images/avatars/clara-gong.jpg'
+        picture: '/images/avatars/clara-gong.jpg'
     },
     Anson_Cook: {
         name: 'Anson Cook',
