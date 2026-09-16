77b91adc1100b094809879516363043d48bc39c1
Megan Steeley <megan.steeley12@gmail.com>
2026-09-16 14:48:09 -0400

    Updating packages to new versons for found vulnerabilities

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,6 @@
 import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { docsLoader, i18nLoader } from '@astrojs/starlight/loaders';
+import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 import { blogSchema } from 'starlight-blog/schema'
 import { starlightTagsExtension } from 'starlight-tags';
 // import { topicSchema } from 'starlight-sidebar-topics/schema'
@@ -13,4 +13,5 @@ export const collections = {
 			// extend: (context) => blogSchema(context).extend(topicSchema.shape)
 		})
 	}),
+	i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
 };
